12871f38c8d014be1f340a11ee9a89857ddeca16
Coder12314 <167703438+Coder12314@users.noreply.github.com>
2024-04-21 13:21:02 -0400

    Add files via upload

--- Patches/legal.cs
+++ Patches/legal.cs
@@ -1,10 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Utilla;
 
 namespace Coders_Mod_Menu.Patches
 {
+    [ModdedGamemode]
     internal class legal_stuff
     {
+       public static bool inAllowedRoom = false;
+
+        [ModdedGamemodeJoin]
+        private void RoomJoined(string gamemode)
+        {
+            inAllowedRoom = true;
+        }
+
+        [ModdedGamemodeLeave]
+        private void RoomLeft(string gamemode)
+        {
+            inAllowedRoom = false;
+        }
     }
 }
